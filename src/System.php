d3ff75a4028f9a3ac59771b27aad90f45bf3c93c
Vlasta Neubauer <paranoiq@centrum.cz>
2018-02-22 17:45:42 +0100

    Strict everything

--- src/System.php
+++ src/System.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types = 1);
 
 namespace Dogma\Tools\Dumper;
 
@@ -7,7 +7,7 @@ class System
 
     public static function isWindows(): bool
     {
-        return strstr(strtolower(PHP_OS), 'win');
+        return strstr(strtolower(PHP_OS), 'win') !== null;
     }
 
 }
